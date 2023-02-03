92db7e3b6927cf7aabe51181fb171154919a2a5c
RaphaelCMiranda <raphael.miranda@pravaler.com.br>
2023-02-03 20:03:28 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import http from "node:http";
+import { json } from "./middleware/json.js";
+import { routes } from "./routes.js";
+import { extractQueryParams } from "./util/extract-query-params.js";
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await json(req, res);
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
+
+  if (route) {
+    const routeParams = req.url.match(route.path);
+
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+
+    req.query = query ? extractQueryParams(query) : {};
+
+    req.params = { ...routeParams.groups };
+
+    return route.handler(req, res);
+  }
+
+  return res.writeHead(404).end();
+});
+
+server.listen(3333);
